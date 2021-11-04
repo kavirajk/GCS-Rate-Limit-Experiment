c31cfab960184c2fc1d735d937ac97be11a00f48
Jordan Rushing <jordan.rushing@grafana.com>
2021-11-04 14:46:32 -0500

    Modify the program to run on a timer; add new flags to configure timer

--- s3_rate_limit.go
+++ s3_rate_limit.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/google/uuid"
 	loki_aws "github.com/grafana/loki/pkg/storage/chunk/aws"
-
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -21,6 +20,8 @@ var bucketName = flag.String("bucket", "", "s3 bucket to read/write to.")
 var region = flag.String("region", "", "s3 region.")
 var accessKey = flag.String("access-key", "", "s3 access key")
 var secretKey = flag.String("secret-key", "", "s3 secret key")
+var timeToRun = flag.Uint64("time-to-run", 5, "The amount of time to run the experiment in seconds.")
+var tickInterval = flag.Uint64("tick-interval", 200, "Interval between requests in milliseconds.")
 
 func createS3ObjectClient(bucketName string, region string, accessKey string, secret string) (*loki_aws.S3ObjectClient, error) {
 	conf := loki_aws.S3Config{
@@ -35,8 +36,6 @@ func createS3ObjectClient(bucketName string, region string, accessKey string, se
 	if err != nil {
 		return nil, fmt.Errorf("error: %v", err)
 	}
-
-	fmt.Printf("S3 Client Created: %+v", client)
 	return client, nil
 }
 
@@ -61,26 +60,46 @@ func deleteObjectBatch(client *loki_aws.S3ObjectClient, keys []string) error {
 func main() {
 	flag.Parse()
 
+	var keys []string
+	ticker := time.NewTicker(time.Duration(*tickInterval) * time.Millisecond)
+	done := make(chan bool)
+
 	go func() {
-		// bucketName, region, accessKey, secret
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				client, err := createS3ObjectClient(*bucketName, *region, *accessKey, *secretKey)
+				if err != nil {
+					fmt.Printf("%+v", err)
+					os.Exit(1)
+				}
+
+				id := uuid.New()
+				key := fmt.Sprintf("foo/%s/%s", id, id)
+				keys = append(keys, key)
+
+				if err := putObjectBatch(client, []string{key}); err != nil {
+					fmt.Printf("%+v", err)
+					os.Exit(1)
+				}
+			}
+		}
+	}()
+
+	go func() {
+		// Run the experiment for timeToRun
+		time.Sleep(time.Duration(*timeToRun) * time.Second)
+		ticker.Stop()
+		done <- true
+
+		// Clean up the objects we wrote when we are done
 		client, err := createS3ObjectClient(*bucketName, *region, *accessKey, *secretKey)
 		if err != nil {
 			fmt.Printf("%+v", err)
 			os.Exit(1)
 		}
-
-		// Generate a slice of random object keys
-		var keys []string
-		for i := 0; i < 5; i++ {
-			id := uuid.New()
-			keys = append(keys, fmt.Sprintf("foo/%s/%s", id, id))
-		}
-		// Basic test using Loki object client to put dummy data and delete it after a brief pause
-		if err := putObjectBatch(client, keys); err != nil {
-			fmt.Printf("%+v", err)
-			os.Exit(1)
-		}
-		time.Sleep(15 * time.Second)
 		if err := deleteObjectBatch(client, keys); err != nil {
 			fmt.Printf("%+v", err)
 			os.Exit(1)
